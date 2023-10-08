412530dca189c366948b563ff83586e0f1c71dfc
Chandra Ardiansyah <chandraardiansyah265@gmail.com>
2023-10-08 18:50:55 +0700

    learn routes

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+//Cara lama
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+//Cara baru
+// Route::view('/','home');
+$appName = 'Laravel 9';
+Route::view('/','home',['appName' => $appName]);
+
+/*Perbedaan compact dan array biasa untuk mengirimkan variable 
+Jika compact key dan value harus sama agar tidak terjadi error,
+sedangkan untuk array key dan value bisa berbeda
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('profile', function(){
+    $name = 'Chandra Ardiansyah';
+    return view('profile',[
+        "name" => $name,
+    ]);
 });
